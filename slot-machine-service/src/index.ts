9de1c6906d0b7145059749e96f680a40d7b3a288
Alon Brand <alonbrand01@gmail.com>
2024-07-29 16:13:14 +0300

    add comments to slot machine index file

--- slot-machine-service/src/index.ts
+++ slot-machine-service/src/index.ts
@@ -18,8 +18,9 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/spin/:userId', async (req, res) => {
+  const userId = req.params.userId;
   try {
-    const userId = req.params.userId;
+    /* Decrease user spin by 1 atomically */
     const decSpinResponse = await axios.post(`http://localhost:3001/dec/spins/${userId}`);
 
     if (!decSpinResponse?.data) {
@@ -27,11 +28,10 @@ app.get('/spin/:userId', async (req, res) => {
       return;
     }
 
-    /* Slot Machine logic */
-
-    // Generate randon values:
+    /* Generate randon values */
     const slotMachineValues = Array.from({ length: 3 }, () => Math.floor(Math.random() * 10));
-    // const slotMachineValues = [5, 5, 5]; // For testing
+
+    /* If no three identical digits: return */
     if (!hasThreeIdenticalValues(slotMachineValues)) {
       res.status(200).json({
         info: 'Not your lucky spin :(', 
@@ -40,28 +40,31 @@ app.get('/spin/:userId', async (req, res) => {
       return;
     };
 
-    // Calculate granted points:
+    /* Calculate granted points: */
     const grantedPoints = slotMachineValues?.reduce((acc: number, curr: number) => acc + curr, 0);
 
-    // Update user points
+    /* Update user points atomically */
     const incPointsResponse = await axios.post(`http://localhost:3001/inc/points/${userId}`, {
       points: grantedPoints
     });
 
-    // Calculate rewards:
+    /* Calculate rewards: */ 
     const rewards = calculateRewards(incPointsResponse?.data);
     const incRewards = await axios.post(`http://localhost:3001/inc/${userId}`, rewards);
 
+    /* If failed to update rewards: show error to the client */
     if (!incRewards.data) {
       return res.status(500).json({ error: "Error while setting the rewards!" })
     }
 
+    /* Retrue a JSON with the user info to the client */
     const userInfo = await axios.get(`http://localhost:3001/info/${userId}`);
     res.json({
       info: userInfo?.data
     })
   }
   catch (error) {
+    console.log(error);
     return res.json({ error: "Error in slot machine service" })
   }
 });
