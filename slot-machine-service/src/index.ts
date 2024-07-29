d5701864eec5f3d434ecd79757fc35d04a7308ce
Alon Brand <alonbrand01@gmail.com>
2024-07-29 15:52:52 +0300

    Update user manager port

--- slot-machine-service/src/index.ts
+++ slot-machine-service/src/index.ts
@@ -20,7 +20,8 @@ app.use(express.json());
 app.get('/spin/:userId', async (req, res) => {
   try {
     const userId = req.params.userId;
-    const decSpinResponse = await axios.post(`http://localhost:3002/dec/spins/${userId}`);
+    console.log("🚀 ~ app.get ~ userId:", userId)
+    const decSpinResponse = await axios.post(`http://localhost:3001/dec/spins/${userId}`);
 
     if (!decSpinResponse?.data) {
       res.status(400).json({ error: 'Not enough spins :(' });
@@ -33,7 +34,7 @@ app.get('/spin/:userId', async (req, res) => {
     const slotMachineValues = Array.from({ length: 3 }, () => Math.floor(Math.random() * 10));
     // const slotMachineValues = [5, 5, 5]; // For testing
     if (!hasThreeIdenticalValues(slotMachineValues)) {
-      res.status(200).json({ error: 'Not your lucky spin :(' });
+      res.status(200).json({ info: 'Not your lucky spin :(' });
       return;
     };
 
@@ -41,19 +42,19 @@ app.get('/spin/:userId', async (req, res) => {
     const grantedPoints = slotMachineValues?.reduce((acc: number, curr: number) => acc + curr, 0);
 
     // Update user points
-    const incPointsResponse = await axios.post(`http://localhost:3002/inc/points/${userId}`, {
+    const incPointsResponse = await axios.post(`http://localhost:3001/inc/points/${userId}`, {
       points: grantedPoints
     });
 
     // Calculate rewards:
     const rewards = calculateRewards(incPointsResponse?.data);
-    const incRewards = await axios.post(`http://localhost:3002/inc/${userId}`, rewards);
+    const incRewards = await axios.post(`http://localhost:3001/inc/${userId}`, rewards);
 
     if (!incRewards.data) {
       return res.status(500).json({ error: "Error while setting the rewards!" })
     }
 
-    const userInfo = await axios.get(`http://localhost:3002/info/${userId}`);
+    const userInfo = await axios.get(`http://localhost:3001/info/${userId}`);
     res.json({
       info: userInfo?.data
     })
